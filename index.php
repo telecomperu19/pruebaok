7eaaaae26f05eb98d18c78c5324434d95b1056ca
telecomperu19 <povisit@cibertec.edu.pe>
2025-02-20 18:52:38 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include "templates/header.php"; ?>
+
+<div class="center-align">
+	<ul>
+		<div class="left-align">
+			<li><a href="read.php"><strong>View Catalog</strong></a></li>
+			<li><a href="insert.php"><strong>Add a Product</strong></a></li>
+			<li><a href="update.php"><strong>Update a Product</strong></a></li>
+			<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
+		</div>
+	</ul>
+</div>
+
+
+<?php include "templates/footer.php"; ?>
